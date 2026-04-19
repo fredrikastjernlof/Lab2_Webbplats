c3f48e6bbdaf88241e50517658ab6687f71f9d3d
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-04-19 13:21:01 +0200

    Display fetched work experiences in list

--- src/main.js
+++ src/main.js
@@ -2,12 +2,30 @@
 
 const url = "https://lab2-webbtjanst.onrender.com/workexperience";
 
+// Gör Fetch-anrop som hämtar alla poster i databasen
 async function getExperiences() {
   try {
     const response = await fetch(url);
     const data = await response.json();
 
-    console.log(data);
+    const list = document.getElementById("experience-list");
+
+    list.innerHTML = ""; // töm listan först
+
+    // Loopa genom alla objekt och skriv ut i DOM
+    data.forEach((item) => {
+      const li = document.createElement("li");
+      li.classList.add("experience-item");
+
+      li.innerHTML = `
+        <h3>${item.companyname}</h3>
+        <p>${item.jobtitle} - ${item.location}</p>
+        <p>${item.startdate} - ${item.enddate || "Pågående"}</p>
+        <p>${item.description}</p>
+      `;
+
+      list.appendChild(li);
+    });
 
   } catch (error) {
     console.error("Fel vid hämtning:", error);
