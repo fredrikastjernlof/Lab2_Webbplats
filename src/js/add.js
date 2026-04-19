9aed624760656e1f5a60aadc8a17f1b1cf8c789e
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-04-19 13:57:59 +0200

    Collect form input values on submit

--- src/js/add.js
+++ src/js/add.js
@@ -3,13 +3,33 @@
 // Funktion som sätter submit-lyssnaren
 export function initAddForm() {
 
+    // Hämta formuläret
     const form = document.getElementById("workexperience-form");
 
+    //Eventlyssnare
     if (form) {
         form.addEventListener("submit", (e) => {
             e.preventDefault();
-            console.log("submit fungerar");
+
+            // Hämta input
+            const companyname = document.getElementById("companyname").value;
+            const jobtitle = document.getElementById("jobtitle").value;
+            const location = document.getElementById("location").value;
+            const startdate = document.getElementById("startdate").value;
+            const enddate = document.getElementById("enddate").value;
+            const ongoing = document.getElementById("ongoing").checked;
+            const description = document.getElementById("description").value;
+
+            //Logga resultatet
+            console.log({
+                companyname,
+                jobtitle,
+                location,
+                startdate,
+                enddate,
+                ongoing,
+                description
+            });
         });
     }
-
 }
