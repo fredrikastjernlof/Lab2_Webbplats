f8b0edcc06f20aa9d1ee5c8436aa524a2649b6dd
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-04-20 14:59:42 +0200

    Configure Vite for multi-page build setup

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+      build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                add: resolve(__dirname, "add.html"),
+                about: resolve(__dirname, "about.html"),
+                edit: resolve(__dirname, "edit.html")
+            },
+        },
+    },
+});
